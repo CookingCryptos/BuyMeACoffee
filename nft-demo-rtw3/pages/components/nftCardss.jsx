e05ad3711bb027235aea0dbb287a709e24bab60c
CookingCryptos <cookingcryptos@protonmail.com>
2022-09-16 19:32:40 -1000

    first challenge

---
+++ nft-demo-rtw3/pages/components/nftCardss.jsx
@@ -0,0 +1,54 @@
+import { IconName } from "react-icons/fa";
+export const NFTCard = ({ nft }) => {
+
+    const [copySuccess, setCopySuccess] = useState('');
+
+    function copyToClipboard(e) {
+        copyTextToClipboard();
+        e.target.focus();
+        setCopySuccess('Copied!');
+      };
+
+    async function copyTextToClipboard() {
+        try {
+            await navigator.clipboard.writeText(nft.contract.address);
+            console.log('Text copied to clipboard');
+        } catch (err) {
+            console.error('Error in copying text: ', err);
+        }
+    }
+
+      
+
+    return (
+        <div className="w-1/4 flex flex-col ">
+        <div className="rounded-md">
+            <img className="object-cover h-128 w-full rounded-t-md" src={nft.media[0].gateway} ></img>
+        </div>
+        <div className="flex flex-col y-gap-2 px-2 py-3 bg-slate-100 rounded-b-md h-110 ">
+            <div className="">
+                <h2 className="text-xl text-gray-800">{nft.title}</h2>
+                <p className="text-gray-600">Id: {nft.id.tokenId.substr(nft.id.tokenId.length - 4)}</p>
+                <p className="text-gray-600" >{`${nft.contract.address.substr(0, 4)}...${nft.contract.address.substr(nft.contract.address.length - 4)}`}</p>
+                
+
+                <button onclick="copyToClipboard()">
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path d="M3.5 3.75a.25.25 0 01.25-.25h13.5a.25.25 0 01.25.25v10a.75.75 0 001.5 0v-10A1.75 1.75 0 0017.25 2H3.75A1.75 1.75 0 002 3.75v16.5c0 .966.784 1.75 1.75 1.75h7a.75.75 0 000-1.5h-7a.25.25 0 01-.25-.25V3.75z"></path><path d="M6.25 7a.75.75 0 000 1.5h8.5a.75.75 0 000-1.5h-8.5zm-.75 4.75a.75.75 0 01.75-.75h4.5a.75.75 0 010 1.5h-4.5a.75.75 0 01-.75-.75zm16.28 4.53a.75.75 0 10-1.06-1.06l-4.97 4.97-1.97-1.97a.75.75 0 10-1.06 1.06l2.5 2.5a.75.75 0 001.06 0l5.5-5.5z"></path></svg>
+</button>
+<br/>
+                {copySuccess}
+
+
+            </div>
+
+            <div className="flex-grow mt-2">
+                <p className="text-gray-600">{nft.description?.substr(0, 150)}</p>
+            </div>
+            <div className="flex justify-center mb-1">
+                <a target={"_blank"} href={`https://etherscan.io/token/${nft.contract.address}`} className="py-2 px-4 bg-blue-500 w-1/2 text-center rounded-m text-white cursor-pointer">View on etherscan</a>
+            </div>
+        </div>
+
+    </div>
+    )
+}
